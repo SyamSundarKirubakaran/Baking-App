496132ad75e78d5e5490e81978c9e932d6b9149b
Syam Sundar K <k.syamsundar21@gmail.com>
2018-01-14 13:38:08 +0530

    Home grid view complete

---
+++ app/src/main/java/com/bugscript/bakingapp/HomeAdapter.java
@@ -0,0 +1,49 @@
+package com.bugscript.bakingapp;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by syamsundark on 14/01/18.
+ */
+
+public class HomeAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+
+    public HomeAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        CardView cardView;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            cardView = (CardView) inflater.inflate(R.layout.item_card, parent, false);
+        } else {
+            cardView = (CardView) convertView;
+        }
+        return cardView;
+    }
+}
--- app/src/main/java/com/bugscript/bakingapp/MainActivity.java
+++ app/src/main/java/com/bugscript/bakingapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.bugscript.bakingapp;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView gridview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridview = findViewById(R.id.gridview);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            gridview.setNumColumns(2);
+        }
+        gridview.setAdapter(new HomeAdapter(MainActivity.this));
     }
 }
