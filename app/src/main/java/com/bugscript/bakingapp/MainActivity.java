e7cb24b2333cf35563c7c84dbf83c7b5c171efde
Syam Sundar K <k.syamsundar21@gmail.com>
2018-01-20 22:49:54 +0530

    network data in home activity

--- app/src/main/java/com/bugscript/bakingapp/MainActivity.java
+++ app/src/main/java/com/bugscript/bakingapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.bugscript.bakingapp;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -11,6 +13,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.bugscript.bakingapp.Description.DetailedList;
+import com.bugscript.bakingapp.Utilities.NetworkUtils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,6 +21,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     public static boolean tabletSize=false;
@@ -35,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     public static String [][] videoURL;
     public static String [][] thumbnailURL;
     public static String [] servings;
+    private URL url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,60 +51,84 @@ public class MainActivity extends AppCompatActivity {
             gridview.setNumColumns(2);
         if(tabletSize && getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
             gridview.setNumColumns(3);
-        getContentsFromJson();
+        try{
+            url=new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
+        }catch (Exception e){
+            Toast.makeText(MainActivity.this,"URL not Recognized..",Toast.LENGTH_LONG).show();
+        }
+        new GetRecipies().execute(url);
     }
 
-    private void getContentsFromJson()  {
-        try {
-            final JSONArray jsonArray = new JSONArray(loadJSONFromAssert());
-            dishNames=new String[jsonArray.length()];
-            servings=new String[jsonArray.length()];
-            ing_numbers=new int[jsonArray.length()];
-            step_numbers=new int[jsonArray.length()];
-            for(int i=0;i<jsonArray.length();i++){
-                JSONObject jo=jsonArray.getJSONObject(i);
-                dishNames[i]=jo.getString("name");
-                servings[i]=jo.getString("servings");
-                JSONArray jsonArray1=jo.getJSONArray("ingredients");
-                ing_numbers[i]=jsonArray1.length();
-                JSONArray jsonArray2=jo.getJSONArray("steps");
-                step_numbers[i]=jsonArray2.length();
-            }
-            int max=ing_numbers[0];
-            for(int i = 0; i < ing_numbers.length; i++)
-                if(max < ing_numbers[i])
-                    max = ing_numbers[i];
-            quantity=new String[jsonArray.length()][max];
-            measure=new String[jsonArray.length()][max];
-            ingredient=new String[jsonArray.length()][max];
-            max=step_numbers[0];
-            for(int i = 0; i < step_numbers.length; i++)
-                if(max < step_numbers[i])
-                    max = step_numbers[i];
-            step_id=new String[jsonArray.length()][max];
-            shortDescription=new String[jsonArray.length()][max];
-            description=new String[jsonArray.length()][max];
-            videoURL=new String[jsonArray.length()][max];
-            thumbnailURL=new String[jsonArray.length()][max];
-            for(int i=0;i<jsonArray.length();i++){
-                JSONObject jo=jsonArray.getJSONObject(i);
-                JSONArray jsonArray1=jo.getJSONArray("ingredients");
-                for(int j=0;j<jsonArray1.length();j++){
-                    JSONObject je=jsonArray1.getJSONObject(j);
-                    quantity[i][j]=je.getString("quantity");
-                    measure[i][j]=je.getString("measure");
-                    ingredient[i][j]=je.getString("ingredient");
+    public class GetRecipies extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String Results = null;
+            try{
+                Results= NetworkUtils.getResponseFromHttpUrl(searchUrl);
+                try {
+                    final JSONArray jsonArray = new JSONArray(Results);
+                    dishNames=new String[jsonArray.length()];
+                    servings=new String[jsonArray.length()];
+                    ing_numbers=new int[jsonArray.length()];
+                    step_numbers=new int[jsonArray.length()];
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject jo=jsonArray.getJSONObject(i);
+                        dishNames[i]=jo.getString("name");
+                        servings[i]=jo.getString("servings");
+                        JSONArray jsonArray1=jo.getJSONArray("ingredients");
+                        ing_numbers[i]=jsonArray1.length();
+                        JSONArray jsonArray2=jo.getJSONArray("steps");
+                        step_numbers[i]=jsonArray2.length();
+                    }
+                    int max=ing_numbers[0];
+                    for(int i = 0; i < ing_numbers.length; i++)
+                        if(max < ing_numbers[i])
+                            max = ing_numbers[i];
+                    quantity=new String[jsonArray.length()][max];
+                    measure=new String[jsonArray.length()][max];
+                    ingredient=new String[jsonArray.length()][max];
+                    max=step_numbers[0];
+                    for(int i = 0; i < step_numbers.length; i++)
+                        if(max < step_numbers[i])
+                            max = step_numbers[i];
+                    step_id=new String[jsonArray.length()][max];
+                    shortDescription=new String[jsonArray.length()][max];
+                    description=new String[jsonArray.length()][max];
+                    videoURL=new String[jsonArray.length()][max];
+                    thumbnailURL=new String[jsonArray.length()][max];
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject jo=jsonArray.getJSONObject(i);
+                        JSONArray jsonArray1=jo.getJSONArray("ingredients");
+                        for(int j=0;j<jsonArray1.length();j++){
+                            JSONObject je=jsonArray1.getJSONObject(j);
+                            quantity[i][j]=je.getString("quantity");
+                            measure[i][j]=je.getString("measure");
+                            ingredient[i][j]=je.getString("ingredient");
+                        }
+                        JSONArray jsonArray2=jo.getJSONArray("steps");
+                        for (int j=0;j<jsonArray2.length();j++){
+                            JSONObject jw=jsonArray2.getJSONObject(j);
+                            step_id[i][j]=jw.getString("id");
+                            shortDescription[i][j]=jw.getString("shortDescription");
+                            description[i][j]=jw.getString("description");
+                            videoURL[i][j]=jw.getString("videoURL");
+                            thumbnailURL[i][j]=jw.getString("thumbnailURL");
+                        }
+                    }
+                }catch (JSONException e){
+                    Log.d("Message","JSON Parsing Exception");
                 }
-                JSONArray jsonArray2=jo.getJSONArray("steps");
-                for (int j=0;j<jsonArray2.length();j++){
-                    JSONObject jw=jsonArray2.getJSONObject(j);
-                    step_id[i][j]=jw.getString("id");
-                    shortDescription[i][j]=jw.getString("shortDescription");
-                    description[i][j]=jw.getString("description");
-                    videoURL[i][j]=jw.getString("videoURL");
-                    thumbnailURL[i][j]=jw.getString("thumbnailURL");
-                }
+            }catch (IOException e){
+                Log.d("Message","IO Network Exception");
             }
+            return Results;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
             gridview.setAdapter(new HomeAdapter(MainActivity.this));
             gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
@@ -111,24 +140,6 @@ public class MainActivity extends AppCompatActivity {
             });
             progressBar.setVisibility(View.INVISIBLE);
             gridview.setVisibility(View.VISIBLE);
-        }catch (JSONException e){
-            Toast.makeText(MainActivity.this,"JSON parsing Exception",Toast.LENGTH_SHORT).show();
         }
-    }
-
-    public String loadJSONFromAssert(){
-        String json=null;
-        try{
-            InputStream inputStream=this.getAssets().open("baking.json");
-            int size= inputStream.available();
-            byte[] buffer=new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            json=new String(buffer,"UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return json;
     }
 }
