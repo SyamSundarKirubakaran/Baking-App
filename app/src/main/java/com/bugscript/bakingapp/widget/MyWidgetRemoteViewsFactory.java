715dcf66907f735a34b8a17659fea618132bd12a
Syam Sundar K <k.syamsundar21@gmail.com>
2018-01-22 19:56:54 +0530

    Widget added

---
+++ app/src/main/java/com/bugscript/bakingapp/widget/MyWidgetRemoteViewsFactory.java
@@ -0,0 +1,103 @@
+package com.bugscript.bakingapp.widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.bugscript.bakingapp.MainActivity;
+import com.bugscript.bakingapp.R;
+import com.bugscript.bakingapp.provider.ContractClass;
+
+/**
+ * Created by syamsundark on 22/01/18.
+ */
+
+public class MyWidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory{
+    private Context mContext;
+    private Cursor mCursor;
+
+    public MyWidgetRemoteViewsFactory(Context applicationContext, Intent intent) {
+        mContext = applicationContext;
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+        if (mCursor != null) {
+            mCursor.close();
+        }
+
+        String dummy= "1";
+
+        final long identityToken = Binder.clearCallingIdentity();
+        mCursor = mContext.getContentResolver().query(ContractClass.nameClass.CONTENT_URI,
+                null,
+                ContractClass.nameClass.COLUMN_INGRED_KEY + " =?",
+                new String[]{dummy},
+                null
+        );
+
+        Binder.restoreCallingIdentity(identityToken);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCursor == null ? 0 : mCursor.getCount();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        if (position == AdapterView.INVALID_POSITION ||
+                mCursor == null || !mCursor.moveToPosition(position)) {
+            return null;
+        }
+
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.collection_widget_list_item);
+        rv.setTextViewText(R.id.widgetItemTaskNameLabel, mCursor.getString(2));
+        rv.setTextViewText(R.id.quanty,mCursor.getString(4));
+        rv.setTextViewText(R.id.measu,mCursor.getString(3));
+
+        Intent fillInIntent = new Intent();
+        fillInIntent.putExtra(WidgetList.EXTRA_LABEL, mCursor.getString(2));
+        rv.setOnClickFillInIntent(R.id.widgetItemContainer, fillInIntent);
+
+        return rv;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return mCursor.moveToPosition(position) ? mCursor.getLong(0) : position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
+
---
+++ app/src/main/java/com/bugscript/bakingapp/widget/WidgetList.java
@@ -0,0 +1,69 @@
+package com.bugscript.bakingapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+import com.bugscript.bakingapp.R;
+import com.bugscript.bakingapp.services.ChangeTitleService;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WidgetList extends AppWidgetProvider {
+
+    public static final String EXTRA_LABEL = "TASK_TEXT";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_list);
+        SharedPreferences sharedPreferences=context.getSharedPreferences("ingrad_pref",0);
+        String RecentlyUpdated=sharedPreferences.getString("dishName","MR.BAKER");
+
+        views.setTextViewText(R.id.widgetTitleLabel,RecentlyUpdated+"");
+        Intent intent = new Intent(context, MyWidgetRemoteViewsService.class);
+        views.setRemoteAdapter(R.id.widgetListView, intent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        ChangeTitleService.startChanging(context);
+    }
+
+
+    public static void sendRefreshBroadcast(Context context) {
+        Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.setComponent(new ComponentName(context, WidgetList.class));
+        context.sendBroadcast(intent);
+    }
+
+    public static void updateWidgetTitle(Context context,AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (action.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
+            // refresh all your widgets
+            AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+            ComponentName cn = new ComponentName(context, WidgetList.class);
+            mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn), R.id.widgetListView);
+        }
+        super.onReceive(context, intent);
+    }
+}
+
