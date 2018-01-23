bdc6af1cd3a798c23b0e2061bfdac55d7ca5d705
Syam Sundar K <k.syamsundar21@gmail.com>
2018-01-23 19:19:24 +0530

    Network availability fix

--- app/src/main/java/com/bugscript/bakingapp/MainActivity.java
+++ app/src/main/java/com/bugscript/bakingapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.bugscript.bakingapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
+import android.support.design.widget.Snackbar;
 import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -74,12 +78,24 @@ public class MainActivity extends AppCompatActivity {
             gridview.setNumColumns(2);
         if(tabletSize && getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
             gridview.setNumColumns(3);
-        try{
-            url=new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
-        }catch (Exception e){
-            Toast.makeText(MainActivity.this,"URL not Recognized..",Toast.LENGTH_LONG).show();
+        if(isNetworkAvailable()) {
+            try {
+                url = new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "URL not Recognized..", Toast.LENGTH_LONG).show();
+            }
+            new GetRecipies().execute(url);
+        }else{
+            progressBar.setVisibility(View.INVISIBLE);
+            Toast.makeText(MainActivity.this,"No Network Available..",Toast.LENGTH_LONG).show();
         }
-        new GetRecipies().execute(url);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
     public class GetRecipies extends AsyncTask<URL, Void, String>{
